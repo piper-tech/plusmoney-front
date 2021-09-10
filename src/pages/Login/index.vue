1cbc0fe685517cb80aa62c6e324913886d82b525
Amanda Silva <amanda-ms@hotmail.com>
2021-09-10 08:42:06 -0300

    feat: create wrapper header

---
+++ src/pages/Login/index.vue
@@ -0,0 +1,34 @@
+<template>
+	<div class="login">
+		<div class="login__wrapper">
+			<figure class="login__wrapper__figure">
+				<img
+					src="@/assets/ilustration-login.svg"
+					class="login__wrapper__figure__img"
+				/>
+			</figure>
+		</div>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+.login {
+	display: flex;
+	height: 100%;
+	&__wrapper {
+		background-color: #508991;
+		clip-path: polygon(0 0, 100% 0, 83% 100%, 0% 100%);
+		width: 50%;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		padding-right: 45px;
+		&__figure {
+			width: 60%;
+			&__img {
+				width: 100%;
+			}
+		}
+	}
+}
+</style>
