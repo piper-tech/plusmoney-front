d9b265a4ee541789480a8a398d54110b13880d99
Amanda Silva <amanda-ms@hotmail.com>
2021-11-30 20:32:07 -0300

    fix: install express

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const path = require('path')
+const serveStatic = require('serve-static')
+const app = express()
+
+app.use(serveStatic(path.join(__dirname, 'dist')))
+app.listen(3000)
