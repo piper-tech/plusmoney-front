631aea6077629f64ab2aace6602f78aaadf6a3e4
Amanda Silva <amanda-ms@hotmail.com>
2021-12-08 09:50:47 -0300

    fix: update server

--- server.js
+++ server.js
@@ -3,5 +3,12 @@ const path = require('path')
 const serveStatic = require('serve-static')
 const app = express()
 
-app.use(serveStatic(path.join(__dirname, 'dist')))
+// app.use(serveStatic(path.join(__dirname, 'dist')))
+app.use(express.static(path.join(__dirname, '/dist')))
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'index.html'))
+})
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'dist/index.html'))
+})
 app.listen(process.env.PORT || 3000)
